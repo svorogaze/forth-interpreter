dd856b2c3ce3497dd021ef5e24e732e53269819b
sitlle <Vktrt@yandex.ru>
2024-11-13 00:06:51 +0300

    argv[1](input file)

--- src/main.cpp
+++ src/main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char** argv) {
         throw std::logic_error("number of command line arguments arguments doesn't match");
     }
 
-    const std::string code_file(argv[0]);
+    const std::string code_file(argv[1]);
     Preprocessor preprocessor(code_file);
     preprocessor.RemoveComments();
     std::string processed_string = preprocessor.GetCurrentText();
